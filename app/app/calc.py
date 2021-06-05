9161617e27d636722cc17d9c9fdd92602efb10a7
Rishabh <imrishabh95@outlook.com>
2021-06-05 15:18:34 +0530

    Unit testing final

--- app/app/calc.py
+++ app/app/calc.py
@@ -1,5 +1,10 @@
 
 
-def add(x,y):
+def add(x, y):
     '''Add two number together'''
     return x+y
+
+
+def subtract(x, y):
+    '''Add two number together'''
+    return y-x
--- app/app/tests.py
+++ app/app/tests.py
@@ -1,9 +1,13 @@
 from django.test import TestCase
+from app.calc import add, subtract
 
-from app.calc import add
 
 class CalcTests(TestCase):
 
     def test_add_numbers(self):
         '''Test that two numbers add together'''
-        self.assertEqual(add(3,8), 11)
+        self.assertEqual(add(3, 8), 11)
+
+    def test_subtract_numbers(self):
+        '''Test that two numbers add together'''
+        self.assertEqual(subtract(3, 8), 5)
